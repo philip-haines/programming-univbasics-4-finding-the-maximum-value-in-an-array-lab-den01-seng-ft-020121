20a0b140a174105fc2ecdc888fc0cf40f550f9b2
eager-monitor-013e <phil.haines92@gmail.com>
2020-12-21 19:14:34 +0000

    Done.

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,16 @@
 def find_max_value(array)
-  # Add your solution here
+  i = 0
+  largest_number = -1
+  
+  while i < array.length do 
+    
+    if largest_number < array[i]
+      largest_number = array[i]
+    end
+    
+    i += 1
+  end
+  
+   largest_number
 end
+    
